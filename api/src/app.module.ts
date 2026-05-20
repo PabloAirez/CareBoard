21c17968d8f1c2671aebd100449450b5f4c1e3ed
Pablo Santos <pabloadscontato@gmail.com>
2026-05-19 23:23:59 -0300

    Feat: Add class validator + Swagger doc + init TypeORM conf in app module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PacientesModule } from './pacientes/pacientes.module';
 
 @Module({
-  imports: [PacientesModule],
+  imports: [PacientesModule,TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'Careboard',
+      autoLoadEntities: true,
+      synchronize: true,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
