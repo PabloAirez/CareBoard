deafef26f9c6b2f6f82be63dd2896bffbd8cc1b7
Pablo Santos <pabloadscontato@gmail.com>
2026-06-16 19:07:40 -0300

    feat: creating endpoints of hospitals

--- api/src/modules/hospital/application/use-cases/find-all-hospital-use-case.ts
+++ api/src/modules/hospital/application/use-cases/find-all-hospital-use-case.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { HospitalRepository } from '../../domain/repositories/hospital.repository';
+import { Hospital } from '../../domain/entities/hospital.entity';
 
 @Injectable()
 export class FindAllHospitalsUseCase {
   constructor(private readonly repository: HospitalRepository) {}
 
-  execute() {
+  execute(): Promise<Hospital[]> {
     return this.repository.findAll();
   }
 }
--- api/src/modules/hospital/presentation/dto/create-hospital.dto.ts
+++ api/src/modules/hospital/presentation/dto/create-hospital.dto.ts
@@ -1,13 +1,17 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class CreateHospitalDto {
   @IsString()
   @IsNotEmpty()
+  @MaxLength(255)
   nome!: string;
 
   @IsString()
   @IsNotEmpty()
+  @MaxLength(18)
   cnpj!: string;
 
+  @IsString()
+  @IsOptional()
   endereco?: string;
 }
